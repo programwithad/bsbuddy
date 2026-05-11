ddaef925542e1e944e45df9ff2238a5c87beda01
programwithad <adityassuper@gmail.com>
2026-05-11 17:05:35 +0530

    Restrict CORS origins to specific domains

--- main.py
+++ main.py
@@ -33,11 +33,15 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://adityawork.live",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 # --- Pydantic schemas ---
 
